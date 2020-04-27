645fd700058f4bcc9438ae841d0cf6b86618871e
Renan Bezerra Gomes <renanbg90@gmail.com>
2020-04-27 14:53:20 -0300

    implementei metodos http
    implementei os 5 metodos get, getId, post, put e delete no controller

--- src/main/java/com/gft/desafios/api/controller/ProdutoController.java
+++ src/main/java/com/gft/desafios/api/controller/ProdutoController.java
@@ -1,9 +1,15 @@
 package com.gft.desafios.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +17,7 @@ import com.gft.desafios.domain.model.Produto;
 import com.gft.desafios.domain.repository.ProdutoRepository;
 
 @RestController
-@RequestMapping("/lista")
+@RequestMapping("/listaProdutos")
 public class ProdutoController {
 
 	@Autowired
@@ -20,5 +26,35 @@ public class ProdutoController {
 	@GetMapping
 	public List<Produto> buscar(){
 		return produtoRepository.findAll();
+
+	}
+	
+	@GetMapping("/{ProdutoId}")
+	public Optional<Produto> buscaPorId(@PathVariable Long ProdutoId){
+		return produtoRepository.findById(ProdutoId);
+	}
+	
+	@PostMapping
+	public Produto adicionar(@RequestBody Produto produto) {
+		Produto salvar = new Produto();
+		salvar= produtoRepository.save(produto);
+		
+		return salvar;
+	}
+	
+	@PutMapping("/{ProdutoId}")
+	public Produto atualiza(@PathVariable Long ProdutoId, @RequestBody Produto produto){
+		Produto atualizar = new Produto();
+		atualizar= produtoRepository.save(produto);
+		
+		return atualizar;
+	}
+	
+	@DeleteMapping("/{ProdutoId}")
+	public void excluir(@PathVariable Long ProdutoId) {
+
+		produtoRepository.deleteById(ProdutoId);
+	
 	}
 }
+
