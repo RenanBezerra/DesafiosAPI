c6fa4e426e39d44c42b780a395a851233ab2ff89
Renan Bezerra Gomes <renanbg90@gmail.com>
2020-04-27 10:59:13 -0300

    Initial commit

---
+++ src/main/java/com/gft/desafios/DesafiosApiApplication.java
@@ -0,0 +1,13 @@
+package com.gft.desafios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gft/desafios/DesafiosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gft.desafios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
