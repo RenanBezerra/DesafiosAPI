ecbb5e6cf366025d417f5fcfed86b73dc852f468
Renan Bezerra Gomes <renanbg90@gmail.com>
2020-04-30 15:08:01 -0300

    criei a response exceptionhandler
    criei a validação daregra de negocio e customização da validação , as classes negocio exception e Problema e o arquivo message.properties

--- src/main/java/com/gft/desafios/api/controller/ProdutoController.java
+++ src/main/java/com/gft/desafios/api/controller/ProdutoController.java
@@ -3,6 +3,8 @@ package com.gft.desafios.api.controller;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -57,7 +59,7 @@ public class ProdutoController {
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Produto adicionar(@RequestBody Produto produto) {
+	public Produto adicionar(@Valid @RequestBody Produto produto) {
 		
 
 		return produtoService.saveProduto(produto);
